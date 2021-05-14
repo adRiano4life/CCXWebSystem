9492466135af992118afcb51b9e38c6011e9851e
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2021-05-14 16:19:05 +0600

    Init

---
+++ EZParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace EZParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = @"https://info.ccx.kz/ru/announcement?per-page=100";
+            
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//td[text(), 'Казахмыс']");
+            
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.OuterHtml);
+            }
+        }
+    }
+}
