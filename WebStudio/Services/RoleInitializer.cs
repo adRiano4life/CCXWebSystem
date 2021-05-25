1d2e5845fcd7ae697445779764e7061349e2d16c
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2021-05-25 23:19:40 +0600

    #13 Добавлен сервис по инициализации ролей

---
+++ WebStudio/Services/RoleInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebStudio.Models;
+
+namespace WebStudio.Services
+{
+    public class RoleInitializer
+    {
+        public static async Task Initializer(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            string adminEmail = "admin@admin.com";
+            string adminPassword = "Q1w2e3r4t%";
+
+            var roles = new[] {"admin", "user"};
+
+            foreach (var role in roles)
+            {
+                if (await roleManager.FindByNameAsync(role) is null)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            if (await userManager.FindByEmailAsync(adminEmail) is null)
+            {
+                User admin = new User
+                {
+                    Email = adminEmail,
+                    UserName = "admin"
+                };
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
