690561b892cafab3acca24c409f62c143f7ef985
Assylbekova Gulzhan <123gulzhan@gmail.com>
2021-06-01 23:56:58 +0600

    #21 добавила модель, экшн и вьюху для создания поставщика

---
+++ WebStudio/Models/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebStudio.Models
+{
+    public class Supplier
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
---
+++ WebStudio/ViewModels/CreateSupplierViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStudio.ViewModels
+{
+    public class CreateSupplierViewModel
+    {
+        [Required(ErrorMessage = "Поле обязательно")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
+        public string Email { get; set; }
+        public string Website { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
+        public string Tags { get; set; }
+    }
+}
