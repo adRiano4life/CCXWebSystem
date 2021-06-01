dabe434aad8f28a7e9c9f93d8e99c7ec1b1ad724
Тиль Александр <alextil88@gmail.com>
2021-06-01 21:08:12 +0600

    #27 Фильтрация и пагинация карт.

--- WebStudio/Controllers/CardsController.cs
+++ WebStudio/Controllers/CardsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,11 @@ namespace WebStudio.Controllers
             return View(cards.ToPagedList(pageNumber, pageSize));
         }
 
+        /// <summary>
+        /// Детальная информация на карточке
+        /// </summary>
+        /// <param name="cardId">для поиска в базе карт метод принимает её Id</param>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult DetailCard(string cardId)
         {
@@ -45,6 +51,11 @@ namespace WebStudio.Controllers
             return NotFound();
         }
 
+        /// <summary>
+        /// Данный Action помечает карту удалённой
+        /// </summary>
+        /// <param name="cardId">для поиска в базе карт на удаление карты метод принимает её Id</param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> DeleteCard(string cardId)
         {
@@ -61,26 +72,67 @@ namespace WebStudio.Controllers
 
             return RedirectToAction("Index", "Cards");
         }
-        
+
+        /// <summary>
+        /// Отображение карт
+        /// </summary>
+        /// <param name="page">Номер страницы</param>
+        /// <param name="from">С какого числа</param>
+        /// <param name="to">По какое число</param>
+        /// <param name="filter">Выбор фильтрации</param>
+        /// <param name="sort">Сортировка статусов карт</param>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult CardInWork(int? page)
+        public IActionResult GetCardInfo(int? page, DateTime? from, DateTime? to, string filter, CardState sort)
         {
-            List<Card> cards = _db.Cards.Where(c => c.CardState == CardState.Проработка).ToList();
-            int pageSize = 20;
+            List<Card> cards = new List<Card>();
+            
+            switch (sort)
+            {
+                case CardState.Новая:
+                    cards = _db.Cards.Where(c => c.CardState == CardState.Новая).ToList();
+                    ViewBag.sort = CardState.Новая;
+                    break;
+                
+                case CardState.Удалена: 
+                    cards = _db.Cards.Where(c => c.CardState == CardState.Удалена).ToList();
+                    ViewBag.sort = CardState.Удалена;
+                    break;
+                
+                case CardState.Проработка: 
+                    cards = _db.Cards.Where(c => c.CardState == CardState.Проработка).ToList();
+                    ViewBag.sort = CardState.Проработка;
+                    break;
+            }
+            
+            switch (filter)
+            {
+                case "DateOfAcceptingEnd": 
+                    if (from != null || to != null)
+                    {
+                        cards = cards.Where(c => c.DateOfAcceptingEnd >= from && c.DateOfAcceptingEnd <= to).ToList();
+                    }
+                    break;
+                
+                case "DateOfAuctionStart": 
+                    if (from != null || to != null)
+                    {
+                        cards = cards.Where(c => c.DateOfAuctionStart >= from && c.DateOfAuctionStart <= to).ToList();
+                    }
+                    break;
+                
+            }
+            
+            
+            
+            int pageSize = 10;
             int pageNumber = page ?? 1;
 
             return View(cards.ToPagedList(pageNumber, pageSize));
+
+
         }
         
-        [HttpGet]
-        public IActionResult CardDeleted(int? page)
-        {
-            List<Card> cards = _db.Cards.Where(c => c.CardState == CardState.Удалена).ToList();
-            int pageSize = 20;
-            int pageNumber = page ?? 1;
-
-            return View(cards.ToPagedList(pageNumber, pageSize));
-        }
         
     }
 }
