8436b5b57b1e71f96f34b3cab629b40b4873efa8
Тиль Александр <alextil88@gmail.com>
2021-06-01 23:53:56 +0600

    Миграция

--- WebStudio/Migrations/20210601163701_init.cs
+++ WebStudio/Migrations/20210601163701_init.cs
@@ -5,7 +5,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace WebStudio.Migrations
 {
-    public partial class TakeCard : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -217,6 +217,34 @@ namespace WebStudio.Migrations
                         onDelete: ReferentialAction.Restrict);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    DateOfCreate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FilePath = table.Column<string>(type: "text", nullable: true),
+                    ExecutorId = table.Column<string>(type: "text", nullable: true),
+                    CardId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_AspNetUsers_ExecutorId",
+                        column: x => x.ExecutorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Requests_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -263,6 +291,16 @@ namespace WebStudio.Migrations
                 name: "IX_Positions_CardId",
                 table: "Positions",
                 column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_CardId",
+                table: "Requests",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ExecutorId",
+                table: "Requests",
+                column: "ExecutorId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -284,6 +322,9 @@ namespace WebStudio.Migrations
 
             migrationBuilder.DropTable(
                 name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
 
             migrationBuilder.DropTable(
                 name: "AspNetRoles");
