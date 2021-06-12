2db891241cd0455a5ced30e6925f7ccf43c22164
Тиль Александр <alextil88@gmail.com>
2021-06-12 20:16:06 +0600

    #55 Убрал датасидинг.

--- WebStudio/Models/WebStudioContext.cs
+++ WebStudio/Models/WebStudioContext.cs
@@ -1,4 +1,7 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,24 +21,6 @@ namespace WebStudio.Models
         }
         
         
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            
-            modelBuilder.Entity<User>().HasData(new User {Id = "1", Name = "Jake", Surname = "Billson", AvatarPath = $"/Images/Avatars/defaultavatar.jpg", Email = "Jake_Billson@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "2", Name = "Pol", Surname = "Dou", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Pol_Dou@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "3", Name = "Helen", Surname = "Merker", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Helen_Merker@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "4", Name = "Jhon", Surname = "Sohnson", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Jhon_Sohnson@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "5", Name = "Phil", Surname = "Madison", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Phill_Madison@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "6", Name = "Mark", Surname = "Takeson", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Mark_Takeson@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "7", Name = "Max", Surname = "Carlson", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Max_Carlson@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "8", Name = "Caren", Surname = "Jameson", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Caren_Jameson@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "9", Name = "July", Surname = "Pablich", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "July_Pablich@gmail.com"});
-            modelBuilder.Entity<User>().HasData(new User {Id = "10", Name = "Tad", Surname = "Wilkerson", AvatarPath = "/Images/Avatars/defaultavatar.jpg", Email = "Tad_Wilkerson@gmail.com"});
-            
-            
-
-            
-        }
+        
     }
 }
