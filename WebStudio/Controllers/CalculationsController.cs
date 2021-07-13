6de2888d7d965cd3f6032b625ccb12cd78a956ad
Тиль Александр <alextil88@gmail.com>
2021-07-13 23:44:41 +0600

    #106 Удалил со старт.страницы "пирог" (диаграмма), добавление статистики лотов в процентах в колонке справа от меню.

--- WebStudio/Controllers/CalculationsController.cs
+++ WebStudio/Controllers/CalculationsController.cs
@@ -28,58 +28,6 @@ namespace WebStudio.Controllers
 
 
 
-        [HttpGet]
-        public async Task<IActionResult> GetCardAndOffer(string cardNumber, string offerId)
-        {
-            if (!string.IsNullOrEmpty(cardNumber) && !string.IsNullOrEmpty(offerId))
-            {
-                Card card = await _db.Cards.FirstOrDefaultAsync(c => c.Number == cardNumber);
-                Offer offer = await _db.Offers.FirstOrDefaultAsync(o => o.Id == offerId);
-                
-                if (card != null && offer != null)
-                {
-                    CardAndOffersViewModel model = new CardAndOffersViewModel()
-                    {
-                        Card = card,
-                        Offer = offer
-                    };
-                    
-                    return View(model);
-                }
-            }
-            return Content("Такого лота нет в базе.");
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetPositionOffer(string positionId)
-        {
-            if (!string.IsNullOrEmpty(positionId))
-            {
-                OfferPosition position = await _db.OfferPositions.FirstOrDefaultAsync(o => o.Id == positionId);
-                if (position != null)
-                {
-                    return PartialView("OfferPositionViewModel", position);
-                }
-                return Content("В такой КП позий нет.");
-            }
-
-            return Content("Такого Id нет.");
-        }
         
-        [HttpGet]
-        public async Task<IActionResult> GetPositionCard(string positionId)
-        {
-            if (!string.IsNullOrEmpty(positionId))
-            {
-                CardPosition position = await _db.Positions.FirstOrDefaultAsync(o => o.Id == positionId);
-                if (position != null)
-                {
-                    return PartialView("CardPositionViewModel", position);
-                }
-                return Content("В этом лоте позиций нет.");
-            }
-
-            return Content("Такого Id нет.");
-        }
     }
 }
