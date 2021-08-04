70ce473c86ca259b8affad128bbf728983a647e6
Тиль Александр <alextil88@gmail.com>
2021-08-04 20:07:15 +0600

    #63 Тест

--- WebStudio.Tests/AccountControllerTests.cs
+++ WebStudio.Tests/AccountControllerTests.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
-using Moq;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
 using WebStudio.Controllers;
 using WebStudio.Models;
 using WebStudio.Services;
@@ -13,23 +14,22 @@ namespace WebStudio.Tests
 {
     public class AccountControllerTests
     {
+        private WebStudioContext db;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<User> _signInManager;
-        private readonly IHostEnvironment _environment;
-        private readonly FileUploadService _uploadService;
+        private readonly IHostEnvironment _environment = new HostingEnvironment();
+        private readonly FileUploadService _uploadService = new FileUploadService();
+        private ILogger<AccountController> _iLogger;
+        
         
         [Fact]
-        public void RegisterNewUser()
+        public async Task RegisterNewUser()
         {
-            string DefaultConnection = "Server=127.0.0.1;Port=5432;Database=WebStudio;User Id=postgres;Password=123";
+            string DefaultConnection = "Server=127.0.0.1;Port=5432;Database=WebStudio;User Id=postgres;Password=QWEqwe123@";
             var optionsBuilder = new DbContextOptionsBuilder<WebStudioContext>();
             var options = optionsBuilder.UseNpgsql(DefaultConnection).Options;
-            var db = new WebStudioContext(options);
-
-            var mock = new Mock<IUserRepository>();
-
-            //AccountController controller = new AccountController(db, _userManager, _roleManager, _signInManager, _environment, _uploadService);
+            var _db = new WebStudioContext(options);
             
             RegisterViewModel model = new RegisterViewModel()
             {
@@ -42,7 +42,20 @@ namespace WebStudio.Tests
                 AvatarPath = $"/Images/Avatars/defaultavatar.jpg",
             };
 
-            //var result = controller.Register(model);
+            AccountController controller = new AccountController(_db, _userManager, _roleManager, _signInManager, _environment, _uploadService, _iLogger);
+
+            var result = controller.Register(model);
+            
+            User user = await _db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+            Assert.NotNull(user);
+            
+            /*Assert.Equal(user.Email, model.Email);
+            Assert.Equal(user.Name, model.Name);
+            Assert.Equal(user.Surname, model.Surname);
+            Assert.Equal(user.PhoneNumber, model.PhoneNumber);*/
+            
+            
+
         }
     }
 }
