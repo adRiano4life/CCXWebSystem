6f8891c6fcf98bc7c0590081719010d2e1d2b366
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2022-02-22 22:35:26 +0600

    #144 Реализован функционал добавления позиции к добавленной карточке

---
+++ WebStudio/ViewModels/CreatePositionViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebStudio.Models;
+
+namespace WebStudio.ViewModels
+{
+    public class CreatePositionViewModel
+    {
+        [Required(ErrorMessage = "Это поле обязательно для заполнение")]
+        [Display(Name = "Наименование")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Display(Name = "Единица измерения")]
+        public string Measure { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Display(Name = "Количество")]
+        public float Amount { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Display(Name = "Цена за единицу товара")]
+        public decimal UnitPrice { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Display(Name = "Срок поставки")]
+        public string DeliveryTime { get; set; }
+        
+        [Display(Name = "Условия поставки")]
+        public string DeliveryTerms { get; set; }
+        public string CardId { get; set; }
+        public Card Card { get; set; }
+        
+    }
+}
