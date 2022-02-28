7d15465eec8df10a3b7a91b32f5916d22bf9015f
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2022-02-28 18:19:41 +0600

    Выставлена плажка для пользователей для возникновения ошибки

--- WebStudio/Startup.cs
+++ WebStudio/Startup.cs
@@ -53,8 +53,8 @@ namespace WebStudio
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseExceptionHandler("/Home/Error");
+                //app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
